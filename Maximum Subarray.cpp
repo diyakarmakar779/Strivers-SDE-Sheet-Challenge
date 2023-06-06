63ce3299eec1b035a65f366aaa7986bb9e45a7e2
Diya Karmakar <78723316+diyakarmakar779@users.noreply.github.com>
2023-06-06 10:25:21 +0530

    Update Maximum Subarray.cpp

--- Maximum Subarray.cpp
+++ Maximum Subarray.cpp
@@ -1,1 +1,34 @@
+#include <bits/stdc++.h> 
 
+// Optimal approach using Kadane's algorithm
+long long maxSubarraySum(int arr[], int n)
+{
+    long long sum = 0;
+    long long maxi = arr[0];
+
+
+    // 1 2 7 -4 3 2 -10 9 1
+    for(int i=0;i<n;i++)
+    {
+        sum+=arr[i];
+        maxi = max(sum,maxi);
+    
+        if(sum<0)
+        {
+            sum=0;
+        }
+
+    }
+
+    if(maxi<0)
+        maxi = 0;
+
+    return maxi;
+
+    /*
+        Don't write main().
+        Don't read input, it is passed as function argument.    
+        No need to print anything.
+        Taking input and printing output is handled automatically.
+    */
+}
